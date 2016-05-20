4f04cee71f0489c3fa39360e9ecc6f78c8d354bc
Rome <shinronster@gmail.com>
2016-05-20 11:41:28 +0200

    initial commit

---
+++ public_html/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>ExamenDaw</title>
+</head>
+<body>
+<h1>ExamenDaw</h1>
+
+<div>
+<?php
+
+print'hello '.'world';
+
+?>
+</div>
+
+</body>
+</html>
+
